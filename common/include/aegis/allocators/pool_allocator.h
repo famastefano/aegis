552967413a11d742bc19033f555df6349eb33892
Stefano Famà <8192713+famastefano@users.noreply.github.com>
2026-07-04 13:10:25 +0200

    common/allocators/pool_allocator: - Fixed alignment to be at least alignof(T) - Refactored free-list pointer type to be more readable

--- common/include/aegis/allocators/pool_allocator.h
+++ common/include/aegis/allocators/pool_allocator.h
@@ -14,11 +14,15 @@ namespace aegis::allocators
  */
 template <typename T> class PoolAllocator
 {
-    using slot_type_t = typename std::conditional_t<sizeof(void *) >= sizeof(T), void *, T>;
+    using free_list_ptr_t = void *;
+    using slot_type_t     = typename std::conditional_t<sizeof(free_list_ptr_t) >= sizeof(T), free_list_ptr_t, T>;
+    inline static constexpr std::align_val_t slot_alignment = alignof(T) > alignof(free_list_ptr_t)
+                                                                  ? std::align_val_t{alignof(T)}
+                                                                  : std::align_val_t{alignof(free_list_ptr_t)};
     struct arena_deleter_t
     {
         void operator()(slot_type_t *p)
-        { ::operator delete[](p, std::align_val_t{alignof(slot_type_t)}); }
+        { ::operator delete[](p, slot_alignment); }
     };
     using arena_t = typename std::unique_ptr<slot_type_t[], arena_deleter_t>;
 
@@ -36,15 +40,15 @@ template <typename T> class PoolAllocator
     {
         if (free_list_head_)
         {
-            void *slot      = *free_list_head_;
-            free_list_head_ = reinterpret_cast<void **>(*free_list_head_);
+            free_list_ptr_t slot = *free_list_head_;
+            free_list_head_      = reinterpret_cast<free_list_ptr_t *>(*free_list_head_);
             return slot;
         }
         else
         {
             auto &arena = arenas_.emplace_back(make_arena());
             init_free_list(arena);
-            free_list_head_ = reinterpret_cast<void **>(arena.get());
+            free_list_head_ = reinterpret_cast<free_list_ptr_t *>(arena.get());
             return *free_list_head_;
         }
     }
@@ -63,29 +67,29 @@ template <typename T> class PoolAllocator
                 void *next_slot_addr = *slot;
                 if (next_slot_addr > static_cast<void *>(p) || !next_slot_addr)
                 {
-                    *slot                         = reinterpret_cast<void **>(p);
-                    *reinterpret_cast<void **>(p) = next_slot_addr;
+                    *slot                                   = reinterpret_cast<free_list_ptr_t *>(p);
+                    *reinterpret_cast<free_list_ptr_t *>(p) = next_slot_addr;
                     return;
                 }
-                slot = reinterpret_cast<void **>(next_slot_addr);
+                slot = reinterpret_cast<free_list_ptr_t *>(next_slot_addr);
             }
         }
         else
         {
-            free_list_head_ = reinterpret_cast<void **>(p);
+            free_list_head_ = reinterpret_cast<free_list_ptr_t *>(p);
         }
     }
 
   private:
     arena_t make_arena()
     {
-        void *mem = ::operator new[](sizeof(slot_type_t) * slots_per_arena_, std::align_val_t{alignof(slot_type_t)});
+        void *mem = ::operator new[](sizeof(slot_type_t) * slots_per_arena_, slot_alignment);
         return arena_t(static_cast<slot_type_t *>(mem));
     }
 
-    arena_t *find_arena(void *p)
+    arena_t const *find_arena(void *p) const
     {
-        for (auto &arena : arenas_)
+        for (auto const &arena : arenas_)
         {
             void *const begin = arena.get();
             void *const end   = arena.get() + slots_per_arena_;
@@ -101,7 +105,7 @@ template <typename T> class PoolAllocator
         auto const end   = begin + slots_per_arena_;
         while (begin != end)
         {
-            *reinterpret_cast<void **>(begin) = static_cast<void *>(begin + 1);
+            *reinterpret_cast<free_list_ptr_t *>(begin) = static_cast<free_list_ptr_t>(begin + 1);
             ++begin;
         }
     }
@@ -115,18 +119,18 @@ template <typename T> class PoolAllocator
 
             auto const end = static_cast<slot_type_t *>(curr_arena.get()) + slots_per_arena_;
             if (i + 1 < arenas_.size())
-                *reinterpret_cast<void **>(end - 1) = static_cast<void *>(arenas_[i + 1].get());
+                *reinterpret_cast<free_list_ptr_t *>(end - 1) = static_cast<free_list_ptr_t>(arenas_[i + 1].get());
             else
-                *reinterpret_cast<void **>(end - 1) = nullptr;
+                *reinterpret_cast<free_list_ptr_t *>(end - 1) = nullptr;
         }
         if (!arenas_.empty())
         {
-            free_list_head_ = reinterpret_cast<void **>(arenas_.front().get());
+            free_list_head_ = reinterpret_cast<free_list_ptr_t *>(arenas_.front().get());
         }
     }
 
     std::vector<arena_t> arenas_;
-    void               **free_list_head_{};
+    free_list_ptr_t     *free_list_head_{};
     std::uint32_t        slots_per_arena_;
 };
 } // namespace aegis::allocators
